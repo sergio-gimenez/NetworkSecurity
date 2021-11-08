75317e757c6b2264787f528d033ce95f1b777517
Sergio Gimenez <sergiogimenezanton@gmail.com>
2021-11-08 11:18:57 +0100

    Added https in index.js

--- 2-lab/WEBSERVER_VM/webserver/index.js
+++ 2-lab/WEBSERVER_VM/webserver/index.js
@@ -10,20 +10,22 @@ const tlsServerCrt = fs.readFileSync('./tls/webserver.crt.pem');
 
 const app = express();
 
-app.use(logger('dev')); // Log requests (GET, POST, ...)
+app.use(logger('dev'));  // Log requests (GET, POST, ...)
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello Express!</h1>');
+  response.send('<h1>Hello Express!</h1>');
 });
 
+
+const caCert = fs.readFileSync('./tls/cacert.pem')
 const httpsOptions = {
-    key: tlsServerKey,
-    cert: tlsServerCrt,
-    requestCert: true,
-    ca: caCert
+  key: tlsServerKey,
+  cert: tlsServerCrt,
+  requestCert: true,
+  ca: caCert
 };
 
-const caCert = fs.readFleSync('./tls/cacert.pem')
+
 const server = https.createServer(httpsOptions, app);
 
 /**
@@ -36,11 +38,7 @@ server.on('listening', onListening);
  * Event listener for HTTP server "listening" event.
  */
 function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    console.log('Listening on ' + bind);
-
-
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
 }
